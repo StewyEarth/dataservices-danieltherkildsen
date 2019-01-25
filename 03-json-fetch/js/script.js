c97129b2e934518a8db19e2039909d6f179e37d2
Daniel Yde Therkildsen <5430788@RTS-365.dk>
2019-01-25 08:24:10 +0100

    Some changes

--- 03-json-fetch/js/script.js
+++ 03-json-fetch/js/script.js
@@ -1,11 +1,30 @@
 document.addEventListener("DOMContentLoaded",()=>{
-	
-	fetch("http://www.omdbapi.com/?apikey=9a3cc64a&t=cars&plot=full")
-	.then( (response) => {
-		return response.json();
-	})
-	.then((data) => {
-		console.log(data.Title)
-	});
 
+	let filmInfoElem = document.querySelector("#filminfo");
+
+	let buttonContainerElem = document.querySelector("div");
+	
+	buttonContainerElem.addEventListener("click",(event) =>{
+		if (event.target.tagName == "BUTTON"){
+			getinfo(event.target.getAttribute("data"))
+			// console.log(event.target.getAttribute("data"))
+		}
+	})
+
+	getinfo("shrek")
+
+		function getinfo(title){
+			fetch(`http://www.omdbapi.com/?apikey=9a3cc64a&t=${title}&plot=full`)
+			.then( (response) => {
+				return response.json();
+			})
+			.then((data) => {
+				filmInfoElem.innerHTML = `<strong>Title:</strong> ${data.Title}<br> <strong>Released:</strong> ${data.Released}<br><hr><strong>Ratings</strong><br><strong> Imdb</strong> score: ${data.imdbRating}<br>`
+				data.Ratings.forEach(rating => {
+					filmInfoElem.innerHTML += `<strong>${rating.Source}</strong>, Score: ${rating.Value}<br>`
+				});
+				filmInfoElem.innerHTML += `<hr><strong>Description:</strong> ${data.Plot}`
+		
+			});
+		}
 });
